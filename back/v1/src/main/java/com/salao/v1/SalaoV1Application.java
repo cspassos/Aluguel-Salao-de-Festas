b62a93b77b97998ae7b74febd9d11e300669c0a9
cspassos <cspassos@latam.stefanini.com>
2018-05-18 12:55:34 -0300

    criacao do projeto / adicionando comandos para comunicacao com o mysql / dependencia do jackson no pom

---
+++ back/v1/src/main/java/com/salao/v1/SalaoV1Application.java
@@ -0,0 +1,12 @@
+package com.salao.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoV1Application.class, args);
+	}
+}
